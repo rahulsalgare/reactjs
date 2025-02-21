60d4e9f084c5aa27c6dba332c6e2a012f4f0c7a3
rahulsalgare <rsalgare95@gmail.com>
2025-02-21 17:49:44 +0530

    91 : Deriving computed values from other computed values

--- 03-tic-tac-toe-starting-project/src/App.jsx
+++ 03-tic-tac-toe-starting-project/src/App.jsx
@@ -33,11 +33,24 @@ function App() {
     gameBoard[row][col] = player;
   }
 
-  // for (const combination of WINNING_COMBINATIONS) {
-  //   const firstSquareSymbol
-  //   const SecondSquareSymbol
-  //   const ThirdSquareSymbol
-  // }
+  let winner = null;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
@@ -68,6 +81,7 @@ function App() {
             isActive={activePlayer == "O"}
           />
         </ol>
+        {winner && <p>You win {winner}</p>}
         <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
