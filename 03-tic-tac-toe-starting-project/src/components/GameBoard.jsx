672b728b1141adbd4527ab0c0062874d0bc0584e
rahulsalgare <rsalgare95@gmail.com>
2025-02-21 15:18:22 +0530

    82 : Lifting state up (core concept)

--- 03-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 03-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,7 +6,7 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initialGameBoard);
 
   function handleSelectSquare(rowIndex, colIndex) {
@@ -17,9 +17,10 @@ export default function GameBoard() {
       const updatedBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+    onSelectSquare();
   }
 
   return (
