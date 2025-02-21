be8ee6a69a840f93976bee493b73575ec0484063
rahulsalgare <rsalgare95@gmail.com>
2025-02-21 10:31:14 +0530

    75 : Concept repetition - working with state

--- 03-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 03-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <li>
-      <span>
-        <span className="player-name">{name}</span>
+      <span className="player">
+        {!isEditing ? (
+          <span className="player-name">{name}</span>
+        ) : (
+          <input type="text" placeholder={name} />
+        )}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={() => setIsEditing(!isEditing)}>Edit</button>
     </li>
   );
 }
