8115188416add96c070b3b6c91f089f5825fbd8d
rahulsalgare <rsalgare95@gmail.com>
2025-02-21 12:04:56 +0530

    78 : Best practice - updating state based on old state correctly

--- 03-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 03-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -4,7 +4,19 @@ export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    // if your new state depends on your previous state value  you should
+    // not update the state like this
+    // setIsEditing(!isEditing);
+
+    // since react 'schedule' to update the state behind the scene when we
+    // directly call the set function, and if we instantlly call another
+    // setIsEditing(!isEditing), the first one might not have finished
+    // setting the value and so, the second one might not set the value
+    // we'd expect it to set.
+
+    // Doing this instead, react will get the value of isEditing
+    // "At the time of executing "
+    setIsEditing((editing) => !editing);
   }
 
   return (
