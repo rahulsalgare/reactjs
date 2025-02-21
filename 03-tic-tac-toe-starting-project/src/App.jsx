60b4066ef95946327d11881e1dacd3a1716cc861
rahulsalgare <rsalgare95@gmail.com>
2025-02-21 17:06:54 +0530

    85 : Deriving state from props

--- 03-tic-tac-toe-starting-project/src/App.jsx
+++ 03-tic-tac-toe-starting-project/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  function handleSelectSquare(rowIndex, colIndex) {
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
 
     setGameTurns((prevTurns) => {
@@ -41,10 +41,7 @@ function App() {
             isActive={activePlayer == "O"}
           />
         </ol>
-        <GameBoard
-          onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
-        />
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
     </main>
   );
