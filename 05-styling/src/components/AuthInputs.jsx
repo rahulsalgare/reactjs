8c2850ee0f41274d24ee8bc13b72291b27eb6a07
rahulsalgare <rsalgare95@gmail.com>
2025-03-05 16:51:30 +0530

    123: Migrating thedemo app to tailwind css

--- 05-styling/src/components/AuthInputs.jsx
+++ 05-styling/src/components/AuthInputs.jsx
@@ -3,13 +3,6 @@ import { styled } from "styled-components";
 import CustomInput from "./Input";
 import Button from "./Button";
 
-const Controls = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 0.5rem;
-  margin-bottom: 1.5rem;
-`;
-
 export default function AuthInputs() {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredPassword, setEnteredPassword] = useState("");
@@ -31,13 +24,16 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <Controls>
+    <div
+      id="auth-inputs"
+      className="w-full max-w-sm p-8 mx-auto rounded shadow-md bg-gradient-to-b from-stone-700 to-stone-800"
+    >
+      <div className="flex flex-col gap-2 mb-6">
         <CustomInput invalid={emailNotValid} label="Email" />
         <CustomInput invalid={passwordNotValid} label="Password" />
-      </Controls>
-      <div className="actions">
-        <button type="button" className="text-button">
+      </div>
+      <div className="flex justify-end gap-4">
+        <button type="button" className="text-amber-400 hover:text-amber-500">
           Create a new account
         </button>
         <Button onClick={handleLogin}>Sign In</Button>
