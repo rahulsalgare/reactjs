d50cdf8c8f073ec6fb589db6e27cbbb493eaa6ed
rahulsalgare <rsalgare95@gmail.com>
2025-03-05 16:20:14 +0530

    118: Introducing tailwind css for react app styling

--- 05-styling/src/components/Header.jsx
+++ 05-styling/src/components/Header.jsx
@@ -1,46 +1,13 @@
 import logo from "../assets/logo.png";
-import "./Header.css";
-import { styled } from "styled-components";
-
-const StyleHeader = styled.header`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 2rem;
-  margin-bottom: 2rem;
-
-  & img {
-    object-fit: contain;
-    margin-bottom: 2rem;
-    width: 11rem;
-    height: 11rem;
-  }
-
-  & h1 {
-    font-size: 1.5rem;
-    font-weight: 600;
-    letter-spacing: 0.4em;
-    text-align: center;
-    text-transform: uppercase;
-    color: #9a3412;
-    font-family: "Pacifico", cursive;
-    margin: 0;
-  }
-
-  & p {
-    text-align: center;
-    color: #a39191;
-    margin: 0;
-  }
-`;
 
 export default function Header() {
   return (
-    <StyleHeader>
-      <img src={logo} alt="A canvas" />
-      <h1>ReactArt</h1>
+    <header className="flex flex-col items-center mt-8 mb-16">
+      <img src={logo} alt="A canvas" className="mb-8 w-44 h-44" />
+      <h1 className="text-4xl fonrt-semibold tracking-widest text-center uppercase text-amber-800 font-bold">
+        ReactArt
+      </h1>
       <p>A community of artists and art-lovers.</p>
-    </StyleHeader>
+    </header>
   );
 }
