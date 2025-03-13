e7277c5f97dbea9bb2ab0bd312b763f8e47ee634
rahulsalgare <rsalgare95@gmail.com>
2025-03-13 19:10:16 +0530

    173 : Migrating the entire demo project to use the Context api

--- 08-advanced-state-management/src/components/Cart.jsx
+++ 08-advanced-state-management/src/components/Cart.jsx
@@ -1,8 +1,8 @@
-import { use } from "react";
+import { useContext } from "react";
 import { CartContext } from "../store/shopping-cart-context.jsx";
 
-export default function Cart({ onUpdateItemQuantity }) {
-  const { items } = use(CartContext);
+export default function Cart() {
+  const { items, updateItemQuantity } = useContext(CartContext);
 
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
@@ -12,7 +12,7 @@ export default function Cart({ onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
+      f{items.length === 0 && <p>No items in cart!</p>}
       {items.length > 0 && (
         <ul id="cart-items">
           {items.map((item) => {
@@ -25,11 +25,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className="cart-item-actions">
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
