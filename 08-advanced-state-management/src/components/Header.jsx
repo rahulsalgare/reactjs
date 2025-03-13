5c4ea72c6eedc0cf8451569ba6d8662d90acc2fd
rahulsalgare <rsalgare95@gmail.com>
2025-03-13 19:17:41 +0530

    174 : Outsourcing context & state into a separate provider component

--- 08-advanced-state-management/src/components/Header.jsx
+++ 08-advanced-state-management/src/components/Header.jsx
@@ -1,11 +1,11 @@
-import { useRef } from "react";
+import { useRef, useContext } from "react";
 
 import CartModal from "./CartModal.jsx";
 import { CartContext } from "../store/shopping-cart-context.jsx";
 
 export default function Header() {
   const modal = useRef();
-  const { items } = use(CartContext);
+  const { items } = useContext(CartContext);
 
   const cartQuantity = items.length;
 
